150dc66d781f87361a5b06518542399ea02190bb
venkat54638 <venkatsai.anilkumar@minfytech.com>
2025-05-28 11:56:27 +0530

    Update ebs.tf

--- ebs.tf
+++ ebs.tf
@@ -12,12 +12,4 @@ resource "aws_ebs_volume" "app_data" {
   }
 }
 
-# Note: Attaching EBS volumes to Auto Scaling Group instances is complex in Terraform
-# because ASG instance IDs are dynamic. Consider using a user data script or AWS Lambda
-# to attach volumes post-instance launch. Below is a placeholder for manual attachment.
-# resource "aws_volume_attachment" "app_data" {
-#   count = 2
-#   device_name = "/dev/sdf"
-#   volume_id   = aws_ebs_volume.app_data[count.index].id
-#   instance_id = aws_instance.web[count.index].id # Not applicable with ASG
-# }
+
